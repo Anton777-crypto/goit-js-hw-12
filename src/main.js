4bd9a3d6365b8fc3979519ba9b676bd1692912f6
Anton2 Spaine <avetrov392@gmail.com>
2024-11-15 14:25:03 -0800

    add markup

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
 import { fetchImages } from './js/pixabay-api';
 import { renderImages, clearGallery } from './js/render-functions';
 
